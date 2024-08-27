885ecc9d61a32852a4500e0f86e61a5ff84c6897
Nguyễn Thành <ducthanh160103@gmail.com>
2024-08-27 22:35:34 +0700

    update icon view information, edit dsnhanvien

--- src/view/dsnhanvien.js
+++ src/view/dsnhanvien.js
@@ -4,20 +4,42 @@ function DsNhanVien() {
             <div className="contact-form">
                 <div
                     className="row"
-                    style={{ rowGap: "10px", columnGap: "0px" }}
+                    style={{ rowGap: "12px", columnGap: "0px" }}
                 >
-                    <div className="col-md-4">
+                    <div className="col-md-6 col-lg-6 col-xl-4">
                         <div className="item d-flex">
-                            <img
-                                src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-anh-the-dep-e1664379835782.jpg"
-                                alt="avatar"
-                                className="avatar"
-                            />
+                            <figure className="cover__avatar">
+                                <img
+                                    src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-anh-the-dep-e1664379835782.jpg"
+                                    alt="avatar"
+                                    className="avatar"
+                                />
+                            </figure>
                             <div className="info w-100">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <h4 className="employee_name">
                                         Tên nhân viên
                                     </h4>
+                                    <div
+                                        className="d-flex align-items-center"
+                                        style={{
+                                            rowGap: "0px",
+                                            columnGap: "7px",
+                                        }}
+                                    >
+                                        <div className="icon-container">
+                                            <i className="fa-solid fa-eye icon"></i>
+                                            <span className="hover-text">
+                                                Thông tin
+                                            </span>
+                                        </div>
+                                        <div className="icon-container">
+                                            <i class="fa-solid fa-user-pen icon"></i>
+                                            <span className="hover-text">
+                                                Sửa
+                                            </span>
+                                        </div>
+                                    </div>
                                 </div>
                                 <p className="position">Chức vụ</p>
                                 <div className="desc">
@@ -32,18 +54,40 @@ function DsNhanVien() {
                         </div>
                     </div>
 
-                    <div className="col-md-4">
+                    <div className="col-md-6 col-lg-6 col-xl-4">
                         <div className="item d-flex">
-                            <img
-                                src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-anh-the-dep-e1664379835782.jpg"
-                                alt="avatar"
-                                className="avatar"
-                            />
+                            <figure className="cover__avatar">
+                                <img
+                                    src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-anh-the-dep-e1664379835782.jpg"
+                                    alt="avatar"
+                                    className="avatar"
+                                />
+                            </figure>
                             <div className="info w-100">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <h4 className="employee_name">
                                         Tên nhân viên
                                     </h4>
+                                    <div
+                                        className="d-flex align-items-center"
+                                        style={{
+                                            rowGap: "0px",
+                                            columnGap: "7px",
+                                        }}
+                                    >
+                                        <div className="icon-container">
+                                            <i className="fa-solid fa-eye icon"></i>
+                                            <span className="hover-text">
+                                                Thông tin
+                                            </span>
+                                        </div>
+                                        <div className="icon-container">
+                                            <i class="fa-solid fa-user-pen icon"></i>
+                                            <span className="hover-text">
+                                                Sửa
+                                            </span>
+                                        </div>
+                                    </div>
                                 </div>
                                 <p className="position">Chức vụ</p>
                                 <div className="desc">
@@ -58,18 +102,40 @@ function DsNhanVien() {
                         </div>
                     </div>
 
-                    <div className="col-md-4">
+                    <div className="col-md-6 col-lg-6 col-xl-4">
                         <div className="item d-flex">
-                            <img
-                                src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-anh-the-dep-e1664379835782.jpg"
-                                alt="avatar"
-                                className="avatar"
-                            />
+                            <figure className="cover__avatar">
+                                <img
+                                    src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-anh-the-dep-e1664379835782.jpg"
+                                    alt="avatar"
+                                    className="avatar"
+                                />
+                            </figure>
                             <div className="info w-100">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <h4 className="employee_name">
                                         Tên nhân viên
                                     </h4>
+                                    <div
+                                        className="d-flex align-items-center"
+                                        style={{
+                                            rowGap: "0px",
+                                            columnGap: "7px",
+                                        }}
+                                    >
+                                        <div className="icon-container">
+                                            <i className="fa-solid fa-eye icon"></i>
+                                            <span className="hover-text">
+                                                Thông tin
+                                            </span>
+                                        </div>
+                                        <div className="icon-container">
+                                            <i class="fa-solid fa-user-pen icon"></i>
+                                            <span className="hover-text">
+                                                Sửa
+                                            </span>
+                                        </div>
+                                    </div>
                                 </div>
                                 <p className="position">Chức vụ</p>
                                 <div className="desc">
@@ -84,18 +150,40 @@ function DsNhanVien() {
                         </div>
                     </div>
 
-                    <div className="col-md-4">
+                    <div className="col-md-6 col-lg-6 col-xl-4">
                         <div className="item d-flex">
-                            <img
-                                src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-anh-the-dep-e1664379835782.jpg"
-                                alt="avatar"
-                                className="avatar"
-                            />
+                            <figure className="cover__avatar">
+                                <img
+                                    src="https://smilemedia.vn/wp-content/uploads/2022/09/cach-chup-anh-the-dep-e1664379835782.jpg"
+                                    alt="avatar"
+                                    className="avatar"
+                                />
+                            </figure>
                             <div className="info w-100">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <h4 className="employee_name">
                                         Tên nhân viên
                                     </h4>
+                                    <div
+                                        className="d-flex align-items-center"
+                                        style={{
+                                            rowGap: "0px",
+                                            columnGap: "7px",
+                                        }}
+                                    >
+                                        <div className="icon-container">
+                                            <i className="fa-solid fa-eye icon"></i>
+                                            <span className="hover-text">
+                                                Thông tin
+                                            </span>
+                                        </div>
+                                        <div className="icon-container">
+                                            <i class="fa-solid fa-user-pen icon"></i>
+                                            <span className="hover-text">
+                                                Sửa
+                                            </span>
+                                        </div>
+                                    </div>
                                 </div>
                                 <p className="position">Chức vụ</p>
                                 <div className="desc">
